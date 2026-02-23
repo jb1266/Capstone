c5bf3e8896bb9e9556b054d1aae5ac76e3bd1262
jb1266 <122730351+jb1266@users.noreply.github.com>
2026-02-23 16:55:05 -0500

    AppSpec SSM V4

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -23,6 +23,16 @@
 # sudo systemctl enable mysql
 
 
+# Change ownership of the application folder to the ubuntu user
+sudo chown -R ubuntu:ubuntu /home/ubuntu/CapstoneProject
+
+# Make sure all scripts in the scripts folder are executable
+sudo chmod +x /home/ubuntu/CapstoneProject/scripts/*.sh
+
+# Optional: Ensure the web folder is readable
+sudo chmod -R 755 /home/ubuntu/CapstoneProject/public
+
+
 
 # Create directory if it doesn't exist
 sudo mkdir -p /home/ubuntu/CapstoneProject
