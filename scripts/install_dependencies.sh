7d13f974f9a9d67888629cebf3ba0ecdc76c0858
jb1266 <122730351+jb1266@users.noreply.github.com>
2026-02-23 17:25:26 -0500

    AppSpec SSM V6

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -58,10 +58,10 @@ fi
 
 
 # Change ownership of the application folder to the ubuntu user
-sudo chown -R ubuntu:ubuntu /home/ubuntu/CapstoneProject
+# sudo chown -R ubuntu:ubuntu /home/ubuntu/CapstoneProject
 
 # Make sure all scripts in the scripts folder are executable
-sudo chmod +x /home/ubuntu/CapstoneProject/scripts/*.sh
+sudo chmod +x scripts/*.sh
 
 # Optional: Ensure the web folder is readable
 sudo chmod -R 755 /home/ubuntu/CapstoneProject/public
