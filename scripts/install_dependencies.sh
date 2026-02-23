b5d846b70691edc50854aac3fb45dd34aa862d89
jb1266 <122730351+jb1266@users.noreply.github.com>
2026-02-23 18:09:46 -0500

    AppSpec SSM V9

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -57,19 +57,5 @@ else
 fi
 
 
-# Change ownership of the application folder to the ubuntu user
-# sudo chown -R ubuntu:ubuntu /home/ubuntu/CapstoneProject
-
-# Make sure all scripts in the scripts folder are executable
-sudo chmod +x scripts/*.sh
-
-# Optional: Ensure the web folder is readable
-sudo chmod -R 755 /home/ubuntu/CapstoneProject/public
-
-
-#  Verify installations (visible in AWS CodeDeploy logs)
-node -v
-npm -v
-mysql --version
 
 echo "Dependencies installation completed"
